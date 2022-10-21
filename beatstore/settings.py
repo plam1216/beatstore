a9bf6d91f7617592dacb73ac7c2683bd829d9d68
Peter Lam <plam1216@gmail.com>
2022-10-21 03:18:07 -0400

    add authentication

--- beatstore/settings.py
+++ beatstore/settings.py
@@ -118,6 +118,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Add this variable to specify where successful logins should redirect to
+LOGIN_REDIRECT_URL = '/beats/'
+
+# Add this variable to specify where logging out redirects to
+LOGOUT_REDIRECT_URL = '/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 

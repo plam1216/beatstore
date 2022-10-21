78da1b34ed1a4268bc1eac0f111ce8a2ff60c354
Peter Lam <plam1216@gmail.com>
2022-10-21 03:16:39 -0400

    Beat CRUD urls added

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,5 +5,8 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('beats/', views.beats_index, name='beats_index'),
-    path('beats/detail', views.beats_detail, name='beats_detail'),
+    path('beats/<int:beat_id>', views.beats_detail, name='beats_detail'),
+    path('beats/create/', views.BeatCreate.as_view(), name='beats_create'),
+    path('beats/<int:pk>/update/', views.BeatUpdate.as_view(), name='beats_update'),
+    path('beats/<int:pk>/delete/', views.BeatDelete.as_view(), name='beats_delete'),
 ]

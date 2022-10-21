4262fbee9bbf9b9f35c56cfc30a1dece32d4861b
Peter Lam <plam1216@gmail.com>
2022-10-21 03:15:30 -0400

    User and Beat model

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-# from django.urls import reverse
+from django.urls import reverse
 from django.contrib.auth.models import User
 
 # Create your models here.
@@ -22,10 +22,13 @@ class Beat(models.Model):
     url = models.CharField(max_length=200)
 
     # M:M relationship (Many Producers : Many Beats)
-    producers = models.ManyToManyField(Producer)
+    # producers = models.ManyToManyField(Producer)
 
     # 1:M relationship (1 User : Many Beats)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     # def __str__(self):
         # return f'{self.name}; Produced by: ${self.producers}'
+
+    def get_absolute_url(self):
+        return reverse('beats_detail', kwargs={'beat_id': self.id})

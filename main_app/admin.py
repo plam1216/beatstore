af1d4f81a8bd635def7f0a329a510495bcfffe8b
Peter Lam <plam1216@gmail.com>
2022-10-24 12:42:00 -0400

    register models

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Beat
+from .models import Beat, Producer, Comment
 
 # Register your models here.
 admin.site.register(Beat)
+admin.site.register(Producer)
+admin.site.register(Comment)

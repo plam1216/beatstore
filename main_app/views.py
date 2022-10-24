1b7c3b76b227e4a53cff470b9ef315ddeae8bcbe
Peter Lam <plam1216@gmail.com>
2022-10-24 12:40:33 -0400

    add comments, unassoc_prod, my_beats, producers_detail

--- main_app/views.py
+++ main_app/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.views.generic import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth.models import User
 
 # logs in user (GET request)
 from django.contrib.auth import login
@@ -16,9 +17,8 @@ from django.contrib.auth.decorators import login_required
 # authorization for CBV
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-import uuid
-import boto3
 from .models import Beat, Producer
+from .forms import CommentForm
 
 S3_BASE_URL = 'https://s3.us-east-1.amazonaws.com/'
 BUCKET = 'beatstore-titans'
@@ -37,54 +37,61 @@ def signup(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            # add user to database
             user = form.save()
-
             login(request, user)
             return redirect('beats_index')
         else:
             error_message = 'Invalid sign up - try again'
+    
     # A bad GET or POST request so page will rerender 'signup.html' with empty form
     form = UserCreationForm()
     context = {'form': form, 'error_message': error_message}
     return render(request, 'registration/signup.html', context)
 
-
-@login_required
 def beats_index(request):
     beats = Beat.objects.all()
     return render(request, 'beats/index.html', { 'beats': beats })
 
-@login_required
 def beats_detail(request, beat_id):
     beat = Beat.objects.get(id=beat_id)
 
-    # producers a beat doesn't have
-    uncredited_producers = Producer.objects.exclude(id__in = beat.producers.all().values_list('id'))
-    return render(request, 'beats/detail.html', { 'beat': beat, 'producers': uncredited_producers })
+    # instantiate comment form
+    comment_form = CommentForm()
 
-# def add_audio(request, beat_id):
-#     # photo-file will be the "name" attribute on the <input type="file">
-#     audio_file = request.FILES.get('audio-file', None)
-#     if audio_file:
-#         s3 = boto3.client('s3')
-#         # need a unique "key" for S3 / needs image file extension too
-#         key = uuid.uuid4().hex[:6] + audio_file.name[audio_file.name.rfind('.'):]
-#         # just in case something goes wrong
-#         try:
-#             s3.upload_fileobj(audio_file, BUCKET, key)
-#             # build the full url string
-#             url = f"{S3_BASE_URL}{BUCKET}/{key}"
-#             # we can assign to cat_id or cat (if you have a cat object)
-#             audio = Audio(url=url, beat_id=beat_id)
-#             audio.save()
-#         except:
-#             print('An error occurred uploading file to S3')
-#     return redirect('beats_detail', beat_id=beat_id)
+    # get producers a beat doesn't have
+    uncredited_producers = Producer.objects.exclude(id__in = beat.producers.all().values_list('id'))
+    return render(request, 'beats/detail.html', { 'beat': beat, 'producers': uncredited_producers, 'comment_form': comment_form })
+
+def producers_detail(request, producer_id):
+    producer = Producer.objects.get(id=producer_id)
+    producer_beats = producer.beat_set.all()
+    return render(request, 'producers/detail.html', {'producer': producer, 'producer_beats': producer_beats})
+
+@login_required
+def my_beats(request):
+    curr_user = User.objects.get(id=request.user.id)
+    curr_user_beats = curr_user.beat_set.all()
+    return render(request, 'my_beats.html', {'curr_user_beats': curr_user_beats})
 
 @login_required
 def assoc_producer(request, beat_id, producer_id):
     Beat.objects.get(id=beat_id).producers.add(producer_id)
+    return redirect('beats_detail', beat_id=beat_id)
+
+@login_required
+def unassoc_producer(request, beat_id, producer_id):
+    Beat.objects.get(id=beat_id).producers.remove(producer_id)
+    return redirect('beats_detail', beat_id=beat_id)
+
+@login_required
+def add_comment(request, beat_id):
+    # curr_user = request.user
+    form = CommentForm(request.POST)
+    if form.is_valid():
+        new_comment = form.save(commit=False)
+        new_comment.beat_id = beat_id
+        new_comment.save()
+    # return redirect('beats_detail', beat_id=beat_id, curr_user=curr_user)
     return redirect('beats_detail', beat_id=beat_id)
 
 class BeatCreate(LoginRequiredMixin, CreateView):
@@ -115,9 +122,9 @@ class ProducerCreate(LoginRequiredMixin, CreateView):
     model = Producer
     fields = ['name', 'IG', 'twitter', 'tiktok']
 
-class ProducerDetail(LoginRequiredMixin, DetailView):
-    model = Producer
-    template_name = 'producers/detail.html'
+# class ProducerDetail(LoginRequiredMixin, DetailView):
+#     model = Producer
+#     template_name = 'producers/detail.html'
 
 class ProducerUpdate(LoginRequiredMixin, UpdateView):
     model = Producer

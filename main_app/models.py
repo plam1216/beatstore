bd742f1b90481092be51cd57db0ca4bc72004495
Peter Lam <plam1216@gmail.com>
2022-10-23 20:01:25 -0400

    add Comment model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,4 @@
+from tkinter import CASCADE
 from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User
@@ -10,25 +11,41 @@ LEASES = (
 )
 
 class Producer(models.Model):
-    name = models.CharField(max_length=20)
-    ig = models.CharField(max_length=50)
+    name = models.CharField(max_length=50)
+    IG = models.CharField(max_length=50)
+    twitter = models.CharField(max_length=50)
+    tiktok = models.CharField(max_length=50)
     
     def __str__(self):
-        return f'{self.name} @{self.ig}'
+        return f'{self.name}, IG: {self.ig}, Twitter: {self.twitter}, TikTok: {self.tiktok}'
+
+    def get_absolute_url(self):
+        return reverse('producers_detail', kwargs={'pk':self.id})
 
 class Beat(models.Model):
-    name = models.CharField(max_length=100)
-    genre = models.CharField(max_length=20)
-    url = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
+    genre = models.CharField(max_length=50)
+    bpm = models.IntegerField()
+    key = models.CharField(max_length=50)
+    image = models.FileField()
+    audio = models.FileField()
 
     # M:M relationship (Many Producers : Many Beats)
-    # producers = models.ManyToManyField(Producer)
+    producers = models.ManyToManyField(Producer)
 
     # 1:M relationship (1 User : Many Beats)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
-    # def __str__(self):
-        # return f'{self.name}; Produced by: ${self.producers}'
+    def __str__(self):
+        return f'{self.name}, Genre: {self.genre}, BPM: {self.bpm}, Key: {self.key}'
 
     def get_absolute_url(self):
         return reverse('beats_detail', kwargs={'beat_id': self.id})
+
+class Comment(models.Model):
+    message = models.CharField(max_length=200)
+
+    beat = models.ForeignKey(Beat, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return {self.message}
